ef147fa538cd493eabcd06450b062b3da1eeb720
glowredman <35727266+glowredman@users.noreply.github.com>
2023-01-28 21:08:45 +0100

    Eclipse Formatting (#114)
    * Switch to Eclipse JDT formatting
    
    * Remove examplecode bloat
    
    * Add maven/CF dependency deobfuscation
    
    Credits to @FalsePattern
    
    * Apply spotless
    
    * Revert "Switch to Eclipse JDT formatting"
    
    This reverts commit 96104db65b4e2630637e9f05b3bbb8fb3a762531.
    
    * Rerun actions
    
    * spotlessApply (#115)
    
    Co-authored-by: GitHub GTNH Actions <>
    
    ---------
    
    Co-authored-by: Raven Szewczyk <git@eigenraven.me>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/com/myname/mymodid/Config.java
+++ src/main/java/com/myname/mymodid/Config.java
@@ -2,27 +2,15 @@ package com.myname.mymodid;
 
 import java.io.File;
 import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.common.config.Property;
 
 public class Config {
 
-    private static class Defaults {
-        public static final String greeting = "Hello World";
-    }
+    public static String greeting = "Hello World";
 
-    private static class Categories {
-        public static final String general = "general";
-    }
-
-    public static String greeting = Defaults.greeting;
-
-    public static void syncronizeConfiguration(File configFile) {
+    public static void synchronizeConfiguration(File configFile) {
         Configuration configuration = new Configuration(configFile);
-        configuration.load();
 
-        Property greetingProperty =
-                configuration.get(Categories.general, "greeting", Defaults.greeting, "How shall I greet?");
-        greeting = greetingProperty.getString();
+        greeting = configuration.getString("greeting", Configuration.CATEGORY_GENERAL, greeting, "How shall I greet?");
 
         if (configuration.hasChanged()) {
             configuration.save();
--- src/main/java/com/myname/mymodid/Tags.java
+++ src/main/java/com/myname/mymodid/Tags.java
@@ -1,12 +1,12 @@
 package com.myname.mymodid;
 
-// Use this class for Strings only. Do not import any classes here. It will lead to issues with Mixins if in use!
+// Use this class for Strings only. Do not import any classes here. It will lead
+// to issues with Mixins if in use!
 
 public class Tags {
 
-    // GRADLETOKEN_* will be replaced by your configuration values at build time
-    public static final String MODID = "GRADLETOKEN_MODID";
-    public static final String MODNAME = "GRADLETOKEN_MODNAME";
-    public static final String VERSION = "GRADLETOKEN_VERSION";
-    public static final String GROUPNAME = "GRADLETOKEN_GROUPNAME";
+    public static final String MODID = "examplemod";
+    public static final String MODNAME = "Example Mod";
+    public static final String VERSION = "GRADLETOKEN_VERSION"; // will be replaced by your configuration values at
+    // build time
 }
